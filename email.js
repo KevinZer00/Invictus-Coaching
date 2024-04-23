5de6031d626062d86401f81fc616fa1424a62c16
Kevin Yu <kevinyuzer0@gmail.com>
2024-04-22 19:13:04 -0700

    Modified function to submit form automatically

--- email.js
+++ email.js
@@ -1,24 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Select the form and modal elements from the DOM
     var form = document.querySelector('form');
     var modal = document.getElementById('successModal');
     var closeButton = document.querySelector('.close-button');
 
-    // Add an event listener to handle form submissions
     form.addEventListener('submit', function (e) {
         e.preventDefault(); // Prevent the default form submission
         modal.style.display = 'flex'; // Show the modal
+        
+        // Submit the form after showing the modal
+        setTimeout(function() {
+            form.submit();
+        }, 1000); // Adjust the timeout as needed
     });
 
-    // Add an event listener to the close button for closing the modal
     closeButton.addEventListener('click', function () {
-        modal.style.display = 'none'; // Hide the modal on close button click
+        modal.style.display = 'none'; // Hide the modal on click
     });
 
-    // Add an event listener to close the modal if clicked outside of it
     window.onclick = function (event) {
         if (event.target === modal) {
-            modal.style.display = 'none'; // Hide the modal on outside click
+            modal.style.display = 'none';
         }
     };
 });
